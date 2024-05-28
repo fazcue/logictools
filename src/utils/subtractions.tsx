eca027c33be40706f52413a31240913195cbc906
Facundo Azcue <fazcue@gmail.com>
2024-05-27 22:22:56 -0300

    Fix unsignedSubract

--- src/utils/subtractions.tsx
+++ src/utils/subtractions.tsx
@@ -22,33 +22,23 @@ export const unsignedSubtract = (
 
 	for (const n1 of [...num1].reverse()) {
 		const n2: string = iNum2 >= 0 ? num2[iNum2] : '0'
-
-		let sub: number = 0 - carry
-
-		if (n1 === '1' && n2 === '1' && carry === 1) {
-			sub = 3
-		} else if (n1 === '1' && n2 === '0') {
-			sub += 1
-		} else if (n1 === '0' && n2 === '1') {
-			sub += 2
-		}
+		let sub: number = +n1 - +n2 - carry
 
 		switch (sub) {
+			case -1:
+				res = '1' + res
+				carry = 1
+				break
 			case 0:
 				res = '0' + res
-				carry = 0
+				if (carry === 1) {
+					carry = n1 === '1' ? 0 : 1
+				} else {
+					carry = 0
+				}
 				break
 			case 1:
 				res = '1' + res
-				carry = 0
-				break
-			case 2:
-				res = '1' + res
-				carry = 1
-				break
-			case 3:
-				res = '1' + res
-				carry = 1
 				break
 		}
 
