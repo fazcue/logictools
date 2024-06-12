10448497d68f92197a7ab29b464e2191c7b4465c
Facundo Azcue <fazcue@gmail.com>
2024-06-12 17:26:25 -0300

    Fix styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="en" data-theme="dark">
+		<html lang="en">
 			<body className={inter.className}>
 				<Header />
 				<main className="main">{children}</main>
